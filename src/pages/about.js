505291dac1ceaa096f73e32a71c0debed803f911
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-14 22:33:08 +0700

    add about.js, tweak index.js

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const AboutUs = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <h1>About Us</h1>
+    <img
+      src="https://images.dog.ceo/breeds/newfoundland/n02111277_5845.jpg"
+      alt="woof1"
+    />
+    <img
+      src="https://images.dog.ceo/breeds/airedale/n02096051_1669.jpg"
+      alt="woof1"
+    />
+  </Layout>
+)
+
+export default AboutUs
