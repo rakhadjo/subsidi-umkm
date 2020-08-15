e42ab5642de62135992cb2b926f5631ce3f91cba
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-08-15 21:39:20 +0800

    feature: basic text input component

--- src/components/header.js
+++ src/components/header.js
@@ -9,26 +9,26 @@ const Header = () => {
       marginBottom: '16px',
       boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)"
     }}>
-        <div style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "16px",
-          maxWidth: "700px",
-          margin: "0 auto"
-        }}>
-          <Link to="/" style={{margin: "0 auto"}}>
-            <Paperclip color="black" size={24}  />
-          </Link>
-          <Link to="/" style={{margin: "0 auto"}}>
-            <AlertCircle color="black" size={24} transform="rotate(180)" />
-          </Link>
-          <Link to="/" style={{margin: "0 auto"}}>
-            <Book color="black" size={24} />
-          </Link>
-        </div>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "16px",
+        maxWidth: "700px",
+        margin: "0 auto"
+      }}>
+        <Link to="/" style={{ margin: "0 auto" }}>
+          <Paperclip color="black" size={24} />
+        </Link>
+        <Link to="/informasi" style={{ margin: "0 auto" }}>
+          <AlertCircle color="black" size={24} transform="rotate(180)" />
+        </Link>
+        <Link to="/kalkulator" style={{ margin: "0 auto" }}>
+          <Book color="black" size={24} />
+        </Link>
+      </div>
     </header>
   )
 }
