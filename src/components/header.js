270fe1014ab24d55cb0ddee08382a9be41be6269
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-08-15 22:27:32 +0800

    refactor: styles for header

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,19 @@ import React from 'react'
 import { AlertCircle, Book, Paperclip } from 'react-feather'
 import { Link } from "gatsby"
 
+import "./styles/header.css"
+
 const Header = () => {
   return (
-    <header style={{
-      backgroundColor: "white",
-      marginBottom: '16px',
-      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)"
-    }}>
-      <div style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "16px",
-        maxWidth: "700px",
-        margin: "0 auto"
-      }}>
-        <Link to="/" style={{ margin: "0 auto" }}>
+    <header className="header">
+      <div className="content">
+        <Link to="/" className="content-item">
           <Paperclip color="black" size={24} />
         </Link>
-        <Link to="/informasi" style={{ margin: "0 auto" }}>
+        <Link to="/informasi" className="content-item">
           <AlertCircle color="black" size={24} transform="rotate(180)" />
         </Link>
-        <Link to="/kalkulator" style={{ margin: "0 auto" }}>
+        <Link to="/kalkulator" className="content-item">
           <Book color="black" size={24} />
         </Link>
       </div>
