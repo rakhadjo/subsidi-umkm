a9155a69acd4370afa863d7ea8de4c6e627cebca
Russel Bradley <russelbradleytan@gmail.com>
2020-08-15 23:28:08 +0800

    fix equation, only for table 1, table 2 not applicable

--- src/helpers/subsidy.js
+++ src/helpers/subsidy.js
@@ -1,13 +1,40 @@
 export const calculateSubsidyAmount = (creditAmount, outstandingDebt, daysOfSubsidisation) => {
-  let subsidyPercentage = 0;
-  if (creditAmount <= 500000000) {
-    subsidyPercentage = 0.2
-  } else if (creditAmount <= 10000000000) {
-    subsidyPercentage = 0.3
-  } else {
-    return "Plafon pembiayaan melebihi batas Rp. 10 miliar"
-  }
+  let daysPercentage = 0;
 
-  const result = (outstandingDebt * daysOfSubsidisation) / 360
+    if (creditAmount <= 10000000) {
+
+      if (daysOfSubsidisation > 180) {
+        daysPercentage = 180 * 0.25
+      } else {
+        daysPercentage = daysOfSubsidisation * 0.25
+      }
+
+    } else if (creditAmount <= 500000000) {
+
+      if (daysOfSubsidisation > 180) {
+        daysPercentage = (90 * 0.06) + (90 * 0.03)
+      } else if (daysOfSubsidisation > 90) {
+        daysPercentage = (90 * 0.06) + ((daysOfSubsidisation-90) * 0.03)
+      } else {
+        daysPercentage = daysOfSubsidisation * 0.06
+      }
+
+    } else if (creditAmount <= 10000000000) {
+
+      if (daysOfSubsidisation > 180) {
+        daysPercentage = (90 * 0.03) + (90 * 0.02)
+      } else if (daysOfSubsidisation > 90) {
+        daysPercentage = (90 * 0.03) + ((daysOfSubsidisation-90) * 0.02)
+      } else {
+        daysPercentage = daysOfSubsidisation * 0.03
+      }
+
+    } else {
+
+      return "Plafon pembiayaan melebihi batas Rp. 10 miliar"
+
+    }
+
+  const result = (daysPercentage * outstandingDebt) / 360
   return result
 }
